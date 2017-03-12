67364c95982221e3345661eaaf6f5ecb0fba2582
Ana Goosen <anagoosen@Anas-MacBook-Pro.local>
2017-03-12 12:52:08 -0400

    added updated main.js file

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//code will determine if x and y strings have the same amount of characters 
 var x="Colin";
 var y="Beth";
 var xlength=x.length;
@@ -7,8 +8,9 @@ if (xlength===ylength) {
 } else {
 	console.log(x + " en " + y + " se name is nie ewe lank.");
 }
+//code will determine who is the oldest, how old they are and how old both are together 
 var xage=28;
-var yage=25;
+var yage=29;
 if (xage<yage) {
 	console.log(x + " is jonger as " + y + ". " + y + " is " + yage + ".");
 } else {
@@ -25,12 +27,28 @@ if ((xage===yage)&&(xlength===ylength)) {
 } else {
 	console.log(x + " en " + y + " is nie ewe oud nie en hul name is nie ewe lank nie.");
 }
-//starting arrays and for loops
-var names=["John","Mary","Beth"];
-printNames(names);
-addName("Anita");
-addName("Sarah");
-printNames(names);
+//write javascript code to determine who is the oldest and by how many years older they are, for example: 
+//Jan is 5 years older than Mary
+var years=" years ";
+var year=" year ";
+var ageDiff=Math.abs(xage-yage);
+var yearToPrint="";
+var xage=28;
+var yage=29;
+if (ageDiff===1) {
+	yearToPrint=year;
+} else {
+	yearToPrint=years;
+}
+if (xage>yage) {
+	console.log(x + " is " + ageDiff + yearToPrint + "older than " + y + ".");
+} else {
+	console.log(y + " is " + ageDiff + yearToPrint + "younger than " + x + ".");
+}
+
+
+
+//new array
 
 function printNames (names) {
 	console.log("The names are:"); 
@@ -38,6 +56,31 @@ function printNames (names) {
 		console.log(i + " - " + names[i]);
 	}
 }
+function printAges (ages) {
+	console.log("The ages are: ");
+	for (var i=0; i<ages.length; i=i+1) {
+		console.log(ages[i]);
+	}
+}
+
+function printNameAndAge (names, ages) {
+	console.log("The names and ages are: ");
+	for (var i=0; i<ages.length; i=i+1) {
+		console.log(names[i] + " : " + ages[i]);
+	}
+}
 function addName (newName) {
 	names.push(newName);
 }
+function addAge (newAge) {
+	ages.push(newAge)
+}
+function addNameAndAge (name, age) {
+	names.push(name);
+	ages.push(age);
+}
+//starting arrays and for loops
+var names=["John","Mary","Beth"];
+var ages=[67,23,46];
+addNameAndAge("Anita",27);
+printNameAndAge(names, ages);
